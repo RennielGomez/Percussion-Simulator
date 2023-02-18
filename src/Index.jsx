a962cf2c75718e4c9067f9a544738b3c2ec6913c
Renniel Gomez <88074156+RennielGomez@users.noreply.github.com>
2023-02-18 15:58:46 +0800

    remove auto reload

--- src/Index.jsx
+++ src/Index.jsx
@@ -9,19 +9,16 @@ function Index() {
                     <h1 className='mb-8 font-bold text-4xl'>Pick an Instrument</h1>
                 </div>
                 <ul className='flex flex-col items-center justify-center'>
-                    <Link to={"/drums"} onClick={()=>setTimeout(() => {
-                    window.location.reload()    
-                    },300 )}>
+                    <Link to={"/drums"}>
                         <li className='text-2xl py-2 w-44 text-center border border-t-0 border-b-2 border-x-0 hover:bg-white hover:text-black hover:scale-x-125'>Drums</li>
                     </Link>
-                    <Link to={"/piano"} onClick={()=>setTimeout(() => {
-                    window.location.reload()    
-                    },300 )}>
+//                     <Link to={"/piano"} onClick={()=>setTimeout(() => {
+//                     window.location.reload()    
+//                     },300 )}>
+                    <Link to={"/piano"}>
                         <li className='text-2xl py-2 w-44 text-center border border-t-0 border-b-2 border-x-0 hover:bg-white hover:text-black hover:scale-x-125'>Piano</li>
                     </Link>
-                    <Link to={"/xylophone"} onClick={()=>setTimeout(() => {
-                    window.location.reload()    
-                    },300 )}>
+                    <Link to={"/xylophone"}>
                         <li className='text-2xl py-2 w-44 text-center border border-t-0 border-b-2 border-x-0 hover:bg-white hover:text-black hover:scale-x-125'>Xylophone</li>
                     </Link>
                 </ul>
